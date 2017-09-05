7039186a2c62d2d2dd0e146190c2c5e7176eea43
Stefon Johns <stefonjohns2@gmail.com>
2017-09-05 14:51:42 -0700

    Simple Calculator
    Composed a simple calculator using C# in Vusial Studio.
    --Used double to allow for decimal.

---
+++ Visual Studio Projects/SimpleCalculator/SimpleCalculator/simpleCalculator.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="simpleCalculator.aspx.cs" Inherits="SimpleCalculator.simpleCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+        </div>
+        <p>
+            <span class="auto-style1"><strong>First Value</strong></span>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="oneTextBox" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <span class="auto-style1"><strong>Second Value</strong></span>:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="twoTextBox" runat="server" style="margin-left: 0px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="addButton" runat="server" Height="26px" OnClick="addButton_Click" Text="+" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="subButton" runat="server" OnClick="subButton_Click" Text="-" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="multButton" runat="server" OnClick="multButton_Click" Text="*" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="divdButton" runat="server" OnClick="divdButton_Click" Text="/" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server" OnDataBinding="addButton_Click"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Visual Studio Projects/SimpleCalculator/SimpleCalculator/simpleCalculator.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculator
+{
+    public partial class simpleCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {   //variables need to be assigned to the textBox
+            string a = oneTextBox.Text;
+            string b = twoTextBox.Text;
+            //textBox variables converted to int
+            double aNumber = double.Parse(a);
+            double bNumber = double.Parse(b);
+            //to keep numbers eligible for decimal points, double must be added to the operands of the addResult variable
+            double addResult = aNumber + bNumber;
+            //convert the integer back into a string for the resultLabel
+            resultLabel.Text = addResult.ToString();
+
+        }
+
+        protected void subButton_Click(object sender, EventArgs e)
+        {
+            
+            string c = oneTextBox.Text;
+            string d = twoTextBox.Text;
+            double cNumber = double.Parse(c);
+            double dNumber = double.Parse(d);
+            double subResult = cNumber - dNumber;
+            resultLabel.Text = subResult.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            string g = oneTextBox.Text;
+            string f = twoTextBox.Text;
+            double gNumber = double.Parse(g);
+            double fNumber = double.Parse(f);
+            double multResult = gNumber * fNumber;
+            resultLabel.Text = multResult.ToString();
+        }
+
+        protected void divdButton_Click(object sender, EventArgs e)
+        {
+            string h = oneTextBox.Text;
+            string i = twoTextBox.Text;
+            double hNumber = double.Parse(h);
+            double iNumber = double.Parse(i);
+            double divdResult = hNumber * iNumber;
+            resultLabel.Text = divdResult.ToString();
+        }
+    }
+}
