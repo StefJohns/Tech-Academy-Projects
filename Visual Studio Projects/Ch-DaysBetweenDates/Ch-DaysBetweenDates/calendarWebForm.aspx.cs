7c5cfb0aab800202b36db0c28744b0cb68f7b8d9
Stefon Johns <stefonjohns2@gmail.com>
2017-09-06 11:43:44 -0700

    --Created Calendar even that provides number of days apart from two selected dates.

---
+++ Visual Studio Projects/Ch-DaysBetweenDates/Ch-DaysBetweenDates/calendarWebForm.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ch_DaysBetweenDates
+{
+    public partial class calendarWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstDate = dateOne.SelectedDate;
+            DateTime secDate = dateTwo.SelectedDate;
+
+            int days = (firstDate.DayOfYear >= secDate.DayOfYear) ? firstDate.DayOfYear - secDate.DayOfYear : secDate.DayOfYear - firstDate.DayOfYear;
+
+
+            resultLabel.Text = days.ToString();
+        }
+    }
+}
